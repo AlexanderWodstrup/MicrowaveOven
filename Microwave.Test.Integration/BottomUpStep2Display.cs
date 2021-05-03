df2a5dd708008ac31dec07bfc4e397594a5bde2b
pallemanden <58727279+pallemanden@users.noreply.github.com>
2021-05-03 16:25:20 +0200

    step2 display

--- Microwave.Test.Integration/BottomUpStep2Display.cs
+++ Microwave.Test.Integration/BottomUpStep2Display.cs
@@ -1,4 +1,5 @@
-﻿using Microwave.Classes.Boundary;
+﻿using System.IO;
+using Microwave.Classes.Boundary;
 using Microwave.Classes.Interfaces;
 using NSubstitute;
 using NUnit.Framework;
@@ -10,62 +11,64 @@ namespace Microwave.Test.Integration
     public class BottomUpStep2Display
     {
         private Display sut;
-        private IOutput output;
+        private Output output;
+        private StringWriter stringWriter;
 
         [SetUp]
         public void Setup()
         {
-            output = Substitute.For<IOutput>();
+            output = Substitute.For<Output>();
             sut = new Display(output);
+            stringWriter = new StringWriter();
         }
 
         [Test]
         public void displayShowsTimeCorrectly_ZeroMinZeroSec()
         {
             sut.ShowTime(0, 0);
-            output.Received().OutputLine(Arg.Is<string>(str=>str.Contains("00:00")));
+            Assert.That(stringWriter.ToString().Contains("00:00"));
         }
 
         [Test]
         public void displayShowsTimeCorrectly_ZeroMinSomeSec()
         {
             sut.ShowTime(0, 7);
-            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("00:07")));
+            Assert.That(stringWriter.ToString().Contains("00:07"));
         }
 
         [Test]
         public void displayShowsTimeCorrectly_SomeMinZeroSec()
         {
             sut.ShowTime(4, 0);
-            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("04:00")));
+            Assert.That(stringWriter.ToString().Contains("04:00"));
         }
 
         [Test]
         public void displayShowsTimeCorrectly_SomeMinSomeSec()
         {
             sut.ShowTime(08, 05);
-            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("08:05")));
+            Assert.That(stringWriter.ToString().Contains("08:05"));
         }
 
         [Test]
         public void displayShowsPowerCorrectly_Zero()
         {
             sut.ShowPower(0);
-            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("0 W")));
+            Assert.That(stringWriter.ToString().Contains("0 W"));
         }
 
         [Test]
         public void displayShowsPowerCorrectly_NonZero()
         {
             sut.ShowPower(50);
-            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("50 W")));
+            Assert.That(stringWriter.ToString().Contains("50 W"));
         }
 
         [Test]
         public void displayClearCorrectly()
         {
             sut.Clear();
-            output.Received().OutputLine(Arg.Is<string>(str => str.Contains("cleared")));
+            Assert.That(stringWriter.ToString().Contains("cleared"));
         }
     }
 }
